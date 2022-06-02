12a970e524e046fac7cc61ceb85d65fd91c6bf43
Definitely not xl_83 <LXALT69@outlook.com>
2022-06-02 01:20:34 +0000

    h

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
+var hourText = "Hours"
+var dayText = "Days"
+var minText = "Minutes"
+var secText = "Seconds"
+
 exports.secsMinsHours = (duration, type) => {
     var milliseconds = Math.floor((duration % 1000) / 100),
         seconds = Math.floor((duration / 1000) % 60),
         minutes = Math.floor((duration / (1000 * 60)) % 60),
         hours = Math.floor((duration / (1000 * 60 * 60)));
 
+
     hours = (hours < 10) ? "0" + hours : hours;
     minutes = (minutes < 10) ? "0" + minutes : minutes;
     seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    if (hours == 1) {
+        hourText = "Hour"
+    } else if (minutes == 1) {
+        minText = "Minute"
+    } else if (seconds == 1) {
+        secText = "Second"
+    }
+
+
+
     if (type == "obj") {
         return {
             "hours": hours,
@@ -15,7 +32,7 @@ exports.secsMinsHours = (duration, type) => {
             "milli": milliseconds
         }
     } else {
-        return `${hours} Hours, ${mins} Minutes and ${seconds} Seconds`
+        return `${hours} ${hourText}, ${minutes} ${minText} and ${seconds} ${secText}`
     }
 }
 
@@ -28,6 +45,17 @@ exports.secsMinsHoursDays = (duration, type) => {
     var minutes = Math.floor(hoursms / (60 * 1000));
     var minutesms = duration % (60 * 1000);
     var sec = Math.floor(minutesms / 1000);
+
+    if (hours == 1) {
+        hourText = "Hour"
+    } else if (minutes == 1) {
+        minText = "Minute"
+    } else if (seconds == 1) {
+        secText = "Second"
+    } else if (days == 1) {
+        dayText = "Day"
+    }
+
     if (type == "obj") {
         return {
             "days": days,
@@ -37,7 +65,7 @@ exports.secsMinsHoursDays = (duration, type) => {
             "milli": milliseconds
         }
     } else {
-        return `${days} Days, ${hours} Hours, ${minutes} Minutes and ${sec} Seconds`
-    }
+        return `${days} ${dayText}, ${hours} ${hourText}, ${minutes} ${minText} and ${seconds} ${secText}`
 
+    }
 }
